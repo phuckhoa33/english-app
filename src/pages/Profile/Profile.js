b2ef7bde5fa02f36d85e97aab6a4f7982632677c
Admin <phuckhoa33>
2023-12-10 18:04:14 +0700

    fix

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,11 +1,24 @@
 import classNames from "classnames/bind";
 import Style from "./Profile.module.scss";
 import { useUserContext } from "../../context/UserContext";
+import {useLocation} from 'react-router-dom';
 
 const cx = classNames.bind(Style);
 
 function Profile() {
   const {user} = useUserContext();
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant", // Optional if you want to skip the scrolling animation
+    });
+  }, [pathname]);
+
   return (
     <div
       className={cx(
